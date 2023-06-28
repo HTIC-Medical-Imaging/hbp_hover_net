f8118cab8fe43b61ae0ac5dcef52f1e4c9f65ffa
sskram <ssk.ram@gmail.com>
2023-06-28 15:32:04 +0530

    minor fix

--- create_mmap.py
+++ create_mmap.py
@@ -4,28 +4,28 @@ import os
 from datetime import datetime
 
 if __name__=="__main__":
-    jp2path = sys.argv[1]
-    mmapdir = sys.argv[2]
+    jp2path = sys.argv[1].strip()
+    mmapdir = sys.argv[2].strip()
     use_glymur = False
 
     use_kdu = True
 
     if use_glymur:
-        accessor = Accessor(jp2path.strip())
-        loadtime,flushtime = create_mmap(accessor,mmapdir.strip(),concurrent=True)    
+        accessor = Accessor(jp2path)
+        loadtime,flushtime = create_mmap(accessor,mmapdir,concurrent=True)    
 
     elif use_kdu:
         start = datetime.now()
-        os.system(f'LD_LIBRARY_PATH=$PWD/lib ./kdu_expand -num_threads 64 -i {jp2path.strip()} -o /cache/tmp.tif')
+        os.system(f'LD_LIBRARY_PATH=$PWD/lib ./kdu_expand -num_threads 64 -i {jp2path} -o /cache/tmp.tif')
         loadend = datetime.now()
         accessor = Accessor('/cache/tmp.tif')
-        create_mmap(accessor,mmapdir.strip(),concurrent=False)
+        accessor.imgpath = jp2path # to set nameprefix of _info.pkl
+        create_mmap(accessor, mmapdir, concurrent=False)
         flushend = datetime.now()
         loadtime = loadend - start
         flushtime = flushend - loadend
+        os.unlink('/cache/tmp.tif')
     
     print('load time', loadtime)
     print('flush time', flushtime)
 
-    if use_kdu:
-        os.unlink('/cache/tmp.tif')
