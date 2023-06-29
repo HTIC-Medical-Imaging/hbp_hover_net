8b1aadc4f979c49e86d55e0d11ea059e85ceaf07
sskram <ssk.ram@gmail.com>
2023-06-29 13:43:04 +0530

    missed file in prev commit

--- run_wsi_mmap.sh
+++ run_wsi_mmap.sh
@@ -9,4 +9,4 @@ GPU_LIST=$7
 INPUT_DIR=`dirname $INPUT_JP2`
 
 python create_mmap.py $INPUT_JP2 $INPUT_DIR && python run_infer.py --gpu=$GPU_LIST --nr_types=6 --type_info_path=type_info.json --batch_size=$BATCH_SIZE --model_mode=fast --model_path=$MODEL_PATH  --nr_inference_workers=$N_INF_WORKERS --nr_post_proc_workers=$N_PP_WORKERS \
-    wsi --proc_mag=20 --input_dir=$INPUT_DIR --output_dir=$OUTPUT_DIR --cache_path=$CACHE_PATH --chunk_shape=16400 --save_mask  #--save_thumb
+    wsi --proc_mag=20 --input_dir=$INPUT_DIR --output_dir=$OUTPUT_DIR --cache_path=$CACHE_PATH --chunk_shape=16400 --save_mask --debug  #--save_thumb 
