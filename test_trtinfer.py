20cac62e0c60082942dfce709b2e636fa58d10d6
sskram <ssk.ram@gmail.com>
2023-07-13 16:16:23 +0530

    minor

--- test_trtinfer.py
+++ test_trtinfer.py
@@ -1,6 +1,7 @@
 import tensorrt as trt
 import numpy as np
 import monai_infer.trt_common as common
+import datetime
 
 # https://github.com/NVIDIA/TensorRT/blob/main/samples/python/yolov3_onnx/onnx_to_tensorrt.py
 
@@ -15,10 +16,10 @@ if __name__=="__main__":
     
     engine_path = '/code/hbp_hover_net/weights/monai_zoo/pathology_nuclei_segmentation_classification/models/engine.trt'
 
-    batch_size = 32
+    batch_size = 64
     
     # FIXME: get a batch of tiles using Accessor
-    images = np.random.randn((batch_size,3,256,256),dtype=np.float32) 
+    images = np.random.randn((batch_size,3,256,256)).astype(np.float32) 
 
     output_names = ['nucleus_prediction', 'horizontal_vertical', 'type_prediction']
     output_shapes = [(2,164,164),(2,164,164),(5,164,164)]
@@ -30,8 +31,10 @@ if __name__=="__main__":
         # print("Running inference on image {}...".format(input_image_path))
         # Set host input to the image. The common.do_inference function will copy the input to the GPU before executing.
         inputs[0].host = images
+        tic = datetime.datetime.now()
         trt_outputs = common.do_inference_v2(context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
-
+        toc = datetime.datetime.now()
+        print(toc-tic)
     
     # Before doing post-processing, we need to reshape the outputs as the common.do_inference will give us flat arrays.
     trt_outputs = [output.reshape([allocbatchsiz]+list(shape)) for output, shape in zip(trt_outputs, output_shapes)]
@@ -40,4 +43,3 @@ if __name__=="__main__":
     # FIXME: post the outputs back in wsi size canvas
 
     common.free_buffers(inputs, outputs, stream)
-    
