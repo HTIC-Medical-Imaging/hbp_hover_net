0cd809897fe754c23ce183a0187dc40439504fa4
sskram <ssk.ram@gmail.com>
2023-07-25 16:16:41 +0530

    minor

--- entryPoint.sh
+++ entryPoint.sh
@@ -3,9 +3,13 @@
 scriptname=$1
 keepalive=${@: -1}
 
+echo "entry point->" ${scriptname}
+
 eval $scriptname $@
 
+echo "ended."
 if [ $keepalive == 1 ]
 then
+    echo "keepalive"
     tail -f /dev/null
 fi
--- test_build_dockercontainer.sh
+++ test_build_dockercontainer.sh
@@ -1,2 +1,2 @@
 version=$1
-docker create --name test_$version --ipc=host --gpus all -v /raid/keerthi/data:/data -v /raid/keerthi/cache:/cache hbp1/hovernet_mmap:$version /workspace/entryPoint.sh
+docker create --name test_$version --ipc=host --gpus all -v /raid/keerthi/data:/data -v /raid/keerthi/cache:/cache hbp1/hovernet_mmap:$version /workspace/entryPoint.sh echo "nop" 1
